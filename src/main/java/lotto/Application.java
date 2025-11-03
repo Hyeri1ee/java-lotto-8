a5a2d9ed0aaf7361c6d6439d67f50fd485f839fd
Hyeri1ee <haerizian10@gmail.com>
2025-11-03 23:20:40 +0900

    refactor(LottoController): 로또 run()추가로 Application진입점 최소화
    <body>

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,26 +1,12 @@
 package lotto;
 
-import lotto.constants.Command;
 import lotto.controller.LottoController;
-import lotto.domain.Lotto;
-import lotto.view.Input;
-
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        Input input = new Input();
-        LottoController controller = new LottoController();
 
-        System.out.println(Command.COMMAND_BUY_PRICE.getMessage());
-        int purchaseAmount = input.readPurchaseAmount();
-        
-        int lottoCount = controller.calculateLottoCount(purchaseAmount);
-        List<Lotto> lottos = controller.generateLottos(lottoCount);
-        
-        System.out.println(lottoCount + Command.COMMAND_BOUGHT.getMessage());
-        for (Lotto lotto : lottos) {
-            System.out.println(lotto.getSortedNumbers());
-        }
+        //로또 컨트롤러 생성후 런
+        LottoController controller = new LottoController();
+        controller.run();
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,8 @@
 package lotto.controller;
 
+import lotto.constants.Command;
 import lotto.domain.Lotto;
+import lotto.view.Input;
 import java.util.ArrayList;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
@@ -9,20 +11,46 @@ public class LottoController {
     private static final int LOTTO_PRICE = 1000;
     private List<Lotto> lottos = new ArrayList<>();
 
-    public int calculateLottoCount(int purchaseAmount) {
-        return purchaseAmount / LOTTO_PRICE;
+    //시작 함수
+    public void run() {
+        Input input = new Input();
+        
+        System.out.println(Command.COMMAND_BUY_PRICE.getMessage());
+        int purchaseAmount = input.readPurchaseAmount();//가격
+        
+        int lottoCount = calculateLottoCount(purchaseAmount);
+        generateLottos(lottoCount);
+        printLottos(lottoCount);
     }
 
-    public Lotto generateRandomLotto() {
+    private int calculateLottoCount(int purchaseAmount) {
+        return purchaseAmount / LOTTO_PRICE;//나머지는 버리기
+    }
+
+    //1~45 로또 번호 생성기
+    private Lotto generateRandomLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return new Lotto(numbers);
     }
 
-    public List<Lotto> generateLottos(int count) {
-        List<Lotto> lottos = new ArrayList<>();
+    //count개수만큼 로또 생성기
+    private void generateLottos(int count) {
         for (int i = 0; i < count; i++) {
             lottos.add(generateRandomLotto());
         }
-        return lottos;
+    }
+
+//로또 출력 함수
+    private void printLottos(int lottoCount) {
+        System.out.println(lottoCount + Command.COMMAND_BOUGHT.getMessage());
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getSortedNumbers());
+        }
+        //해당 부분 스트림으로 바꾸면 뭐가 더 나을까
+        /*
+        lottos.stream()
+            .map(Lotto::getSortedNumbers)
+            .forEach(System.out::println);
+        */
     }
 }
